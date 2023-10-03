a97842cdaebfc4aee3f07e6a18a3bf2f012605f2
user <user@LAPTOP-2DK3J1EI>
2023-10-03 11:54:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingresa un numero: ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j < 2 * num; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
